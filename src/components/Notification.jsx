4c18944c98ed3720f7fe8f5c422e854e7cb9f7fb
prakash9724 <hellobrother91069@gmail.com>
2024-11-14 20:39:16 +0530

    Hero section and other small components created

---
+++ src/components/Notification.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notification = ({className,title}) => {
+  return (
+    <div className={`${className}`}>
+      
+    </div>
+  )
+}
+
+export default Notification
